aa4193dc759bee84e7595630a5cbbeeabfaf1122
cdalangin <florenzdalangin@gmail.com>
2021-09-09 23:51:10 -0400

    initial push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Landing from "./sections/Landing"
+import About from "./sections/About"
+
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/sections/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+export default function About() {
+
+    return (
+        <div>
+            <div>
+                <p><b>CUNY Hackathon Fall 2021</b> is the first virtually held hackathon for
+                    the City University of New York students, but we still welcome
+                    students from all over the world. With the goal of bringing together
+                    students in software development, design, marketing and entrepreneurship
+                    to build solutions for problems unique to the New York City community,
+                    this 24-hour event will see students create projects and learn from
+                    workshops and activities.</p>
+            </div>
+            <div>
+
+                {/* Register Buttons */}
+            </div>
+        </div>
+
+    )
+
+}
