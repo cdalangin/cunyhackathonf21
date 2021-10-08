a31b92ca7319f3bec5314d5c549d597f8cf67938
cdalangin <florenzdalangin@gmail.com>
2021-10-07 22:16:55 -0400

    sponsors

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./sections/About"
 import GetInvolved from "./sections/GetInvolved"
 import Tracks from "./sections/Tracks"
 import FAQ from "./sections/FAQ"
+import Sponsors from "./sections/Sponsors"
 import TBA from "./sections/TBA"
 import Footer from "./sections/Footer"
 
@@ -16,6 +17,7 @@ function App() {
       <GetInvolved />
       {/* <Tracks /> */}
       <FAQ />
+      <Sponsors />
       <TBA />
       <Footer />
     </div>
---
+++ src/sections/Sponsors.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "../styles/Sponsors.css"
+import bloomberg from "../assets/sponsors/bloomberg.png"
+import cunystartups from "../assets/sponsors/cunystartups.png"
+import mlh from "../assets/sponsors/mlh.png"
+import mufg from "../assets/sponsors/mufg.jpg"
+import astro2 from "../assets/astronaut2.png"
+
+export default function Sponsors() {
+
+    return (
+        <div className="spon-bg">
+            {/* <img src={astro2} alt="astronaut on rocket" className="spon-astro2" /> */}
+            <h1 className="spon-title">Sponsors and Partners</h1>
+            <div className="spon-logos">
+                <img src={bloomberg} alt="bloomberg" className="spon-logo" />
+                <img src={mufg} alt="mufg" className="spon-logo" />
+                <img src={cunystartups} alt="cunystartups" className="spon-logo" />
+                <img src={mlh} alt="mlh" className="spon-logo" />
+                
+            </div>
+            
+        </div>
+    )
+
+}
